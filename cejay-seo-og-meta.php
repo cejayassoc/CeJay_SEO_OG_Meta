b00658dac8efcc3d8faf88eb79aaa72d9b70e8fb
Lisa Baird <webbiz@cejayassoc.com>
2023-09-19 13:19:43 -0400

    Create cejay-seo-og-meta.php
    Create a folder named "CeJay_SEO_OG_Meta" in your WordPress plugins directory (usually located at wp-content/plugins/) and add this file to it.

---
+++ cejay-seo-og-meta.php
@@ -0,0 +1,53 @@
+<?php
+/*
+Plugin Name: CeJay SEO OG Meta
+Description: This plugin generates meta tags for SEO and Open Graph (OG) using custom fields and defaults.
+Version: 1.0
+Author: Your Name
+*/
+
+// Function to generate SEO and OG meta tags
+function cejay_magic_meta() {
+    global $post;
+    $cstmMeta = get_post_custom(get_the_ID());
+
+    // Set defaults
+    $title = "";
+    $description = "";
+    $defImg = "";
+    $pageURL = get_permalink($post);
+
+    // TITLE
+    if (isset($cstmMeta['cejay-title'][0]) && $cstmMeta['cejay-title'][0] != '') {
+        $title = $cstmMeta['cejay-title'][0];
+    } elseif (get_the_title()) {
+        $title = esc_html(get_the_title());
+    }
+
+    // DESCRIPTION
+    if (isset($cstmMeta['cejay-description'][0]) && $cstmMeta['cejay-description'][0] != '') {
+        $description = $cstmMeta['cejay-description'][0];
+    } elseif (has_excerpt()) {
+        $description = strip_tags(get_the_excerpt());
+    }
+
+    // FEATURED IMAGE
+    $post_thumbnail_id = get_post_thumbnail_id($post);
+    $imageURL = wp_get_attachment_image_url($post_thumbnail_id, 'full');
+    if (isset($imageURL) && $imageURL == '') {
+        $imageURL = $defImg;
+    }
+
+    echo '<meta name="description" content="' . $description . '" />' . "\r\n";
+    echo '<meta name="title" content="' . $title . '" />' . "\r\n";
+    echo '<meta property="og:title" content="' . $title . '" />' . "\r\n";
+    echo '<meta property="og:type" content="website" />' . "\r\n";
+    echo '<meta property="og:url" content="' . $pageURL . '" />' . "\r\n";
+    echo '<meta property="og:image" content="' . $imageURL . '" />' . "\r\n";
+    echo '<meta property="og:description" content="' . $description . '" />' . "\r\n";
+    echo '<meta property="og:locale" content="en_US" />' . "\r\n";
+    echo '<meta property="og:site_name" content="' . get_bloginfo('name') . '" />' . "\r\n";
+}
+
+// Hook the function to the wp_head action to add meta tags to the header
+add_action('wp_head', 'cejay_magic_meta');
